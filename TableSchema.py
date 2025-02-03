71f16b51c605d65fa976fa6207b57c90e6e3a580
Fabien Herry <fabien_herry@yahoo.fr>
2025-02-03 14:44:01 +0100

    Modification des table ajout constaint

--- TableSchema.py
+++ TableSchema.py
@@ -1,18 +1,19 @@
+Schema = "Herry"
+
 #Recupererable depuis title.basic et ratings
-filmTable = """
-CREATE TABLE IF NOT EXISTS Herry.filmbasic (
+filmTable = f"""
+CREATE TABLE IF NOT EXISTS {Schema}.filmbasic (
     filmId VARCHAR(100) PRIMARY KEY,
     title VARCHAR(500),
     mainGenre VARCHAR(500),
     startYear INT,
     length INT,
-    avg_Rating FLOAT,
-    num_Rating INT
+
 );
 """
 #Recuperable depuis name.basics
-peopleTable = """
-CREATE TABLE Herry.people (
+peopleTable = f"""
+CREATE TABLE {Schema}.people (
     pId VARCHAR(100) PRIMARYKEY,
     name VARCHAR(100),
     yearBirth INT,
@@ -20,10 +21,21 @@ CREATE TABLE Herry.people (
  );"""
 
 #Recuperable depuis title.principals
-jobTable = """
- CREATE TABLE Herry.job (
- jId VARCHAR(100) PRIMARYKEY,
- pId VARCHAR(100),
- filmId VARCHAR(100),
+jobTable = f"""
+ CREATE TABLE {Schema}.job (
+    jId VARCHAR(100) PRIMARYKEY,
+    pId VARCHAR(100),
+    filmId VARCHAR(100),
+    CONSTRAINT Person FOREIGN KEY (pId) REFERENCES people(pId),
+    CONSTRAINT Film FOREIGN KEY (filmID) REFERENCES filmbasic(filmId)
  );
  """
+
+#Recuperable ratings
+ratingsTable = f"""
+ CREATE TABLE {Schema}.ratings (
+    rId VARCHAR(100) PRIMARYKEY,
+    filmId VARCHAR(100),
+    CONSTRAINT Film FOREIGN KEY (filmID) REFERENCES filmbasic(filmId)
+ );
+"""
